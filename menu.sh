d4b353e6e93f60eaa9fe8adc70f135e23fc0a605
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2021-10-28 21:57:57 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,23 +1,66 @@
 #!/bin/bash
 
+banner(){
+echo "
+███████╗███████╗██████╗  ██████╗
+╚══███╔╝██╔════╝██╔══██╗██╔═══██╗
+  ███╔╝ █████╗  ██████╔╝██║   ██║
+ ███╔╝  ██╔══╝  ██╔══██╗██║   ██║
+███████╗███████╗██║  ██║╚██████╔╝
+╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝
+"
+}
+
+start(){
 menu=""
 until [ "$menu" = "10" ]; do
-
 clear
+banner
+cat << EOF
+══════════════════════════════════
+[1]  Install Apps
+[2]  Remove Apps
+[3]  Update ZERO
+══════════════════════════════════
+[X] Exit
+══════════════════════════════════
 
-echo "
-╔═════════════════════════════════════════════════════════════╗
-║  ╔═══════════════════════════════════════════════════════╗  ║
-║  ║                                                       ║  ║
-║  ║            ███████╗███████╗██████╗  ██████╗           ║  ║
-║  ║            ╚══███╔╝██╔════╝██╔══██╗██╔═══██╗          ║  ║
-║  ║              ███╔╝ █████╗  ██████╔╝██║   ██║          ║  ║
-║  ║             ███╔╝  ██╔══╝  ██╔══██╗██║   ██║          ║  ║
-║  ║            ███████╗███████╗██║  ██║╚██████╔╝          ║  ║
-║  ║            ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝           ║  ║
-║  ║                                                       ║  ║
-║  ╚═══════════════════════════════════════════════════════╝  ║
-╚═════════════════════════════════════════════════════════════╝"
+EOF
+read -p "Input Value | [Enter]  : " menu </dev/tty
+
+case $menu in
+
+1)
+install_apps
+;;
+
+2)
+remove_apps
+;;
+
+3)
+update_zero
+;;
+
+x)
+break
+;;
+
+X)
+break
+;;
+
+*) ;;
+
+esac
+done
+}
+
+install_apps(){
+  menu=""
+  until [ "$menu" = "10" ]; do
+  clear
+  banner
 cat << EOF
 
 [1]  Redeploy Portainer
@@ -25,17 +68,15 @@ cat << EOF
 [3]  Redeploy Autoheal
 [4]  Redeploy Watchtower
 [5]  Redeploy Caddy
-═══════════════════════════════════════════════════════════════
+══════════════════════════════════
 [6]  Redeploy Deluge
 [7]  Redeploy Radarr
 [8]  Redeploy Sonarr
 [9]  Redeploy Jackett
 [10] Redeploy Prowlarr
-═══════════════════════════════════════════════════════════════
-
+══════════════════════════════════
 [X] Exit
-
-═══════════════════════════════════════════════════════════════
+══════════════════════════════════
 
 EOF
 read -p "Input Value | [Enter]  : " menu </dev/tty
@@ -104,3 +145,98 @@ break
 
 esac
 done
+}
+
+remove_apps(){
+menu=""
+until [ "$menu" = "10" ]; do
+clear
+banner
+cat << EOF
+
+[1]  Remove Portainer
+[2]  Remove Cloudcmd
+[3]  Remove Autoheal
+[4]  Remove Watchtower
+[5]  Remove Caddy
+══════════════════════════════════
+[6]  Remove Deluge
+[7]  Remove Radarr
+[8]  Remove Sonarr
+[9]  Remove Jackett
+[10] Remove Prowlarr
+══════════════════════════════════
+[X] Exit
+══════════════════════════════════
+
+EOF
+read -p "Input Value | [Enter]  : " menu </dev/tty
+
+case $menu in
+
+1)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" portainer down
+;;
+
+2)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" cloudcmd down
+;;
+
+3)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" autoheal down
+;;
+
+4)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" watchtower down
+;;
+
+5)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" caddy down
+;;
+
+6)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" deluge down
+;;
+
+7)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" radarr down
+;;
+
+8)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" sonarr down
+;;
+
+9)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" jackett down
+;;
+
+10)
+docker-compose --env-file "/opt/zerobox/.env" -f "/opt/zerobox/docker-compose.yml" prowlarr down
+;;
+
+x)
+break
+;;
+
+X)
+break
+;;
+
+*) ;;
+
+esac
+done
+}
+
+update_zero(){
+  clear
+  banner
+  echo "
+  ══════════════════════════════════"
+  mkdir -p "/opt/zerobackup"
+  mv "/opt/zerobox/.env" "/opt/zerobackup"
+  rm -rf "/opt/zerobackup"
+  mkdir -p "/opt/zerobackup"
+  git clone https://github.com/morganzero/zerobox.git "/opt/zerobox"
+  cp "/opt/zerobackup/.env" "/opt/zerobox/"
+}
